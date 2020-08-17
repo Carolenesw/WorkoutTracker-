56fffe08ffddc2fda5409b0b6a9d615b2b1dea76
Carolene Wilson <carolenesw@gmail.com>
2020-08-17 13:42:19 -0400

    require packages and set up file connections

---
+++ app.js
@@ -0,0 +1,28 @@
+// require npm packages
+const express = require("express");
+const mongoose = require("mongoose");
+
+const path = require("path");
+
+// set up connection path
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// create connection using mongoose
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutDB", {
+  useNewUrlParser: true
+});
+
+// connect to routes
+// app.use()
+
+// port for listen connection
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
