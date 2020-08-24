7abd87f452d297f7b775ee9932eedf60946395ae
Carolene Wilson <carolenesw@gmail.com>
2020-08-24 11:22:43 -0400

    find workout db

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,12 @@
 // require router and module file path 
 const router = require("express").Router();
-const Workout = require("../models/models");
+// const Workout = require("../models/models");
 
 const path = require("path");
 
-let db = require("../models/models");
-const { model } = require("mongoose");
+// require db workout model
+let db = require("../models/workout_model");
+// const mongoose = require("mongoose");
 
 // create routes to link html pages/routes
 router.get("/exercise", (req, res) => {
@@ -16,8 +17,19 @@ router.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
 })
 
+// create api routes to database
+router.get("/api/workouts", (req, res) => {
+    console.log("Inside last workout route");
+    db.Workout.find({})
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch (error => {
+        res.status(400).json(error);
+    });
+});
 
 
 
 
-module.exports = router
+module.exports = router;
