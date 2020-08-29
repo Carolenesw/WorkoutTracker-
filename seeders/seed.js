65db825b8011e37d67d57d3c7d875e81bb69e6be
Carolene Wilson <carolenesw@gmail.com>
2020-08-28 21:19:48 -0400

    revert model path

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,5 +1,5 @@
 let mongoose = require("mongoose");
-let db = require("../models/models");
+let db = require("../models/workout_model");
 
 mongoose.connect("mongodb://localhost/workout", {
   useNewUrlParser: true,
